817ce7996b99e8644ee10d1802b24c70ddbc6756
ddubey <dheeraj.dubey@ascenaretail.com>
2019-04-22 15:40:49 +0530

    update 1

---
+++ src/main/java/com/dkd/pc/dkdpetclinic/DkdPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.dkd.pc.dkdpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkdPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkdPetclinicApplication.class, args);
+	}
+
+}
